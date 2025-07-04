b2f8349c7f81836e45f30523c718f5ce37ba38c3
Gopal-G0 <heyitsmegopal@gmail.com>
2025-07-04 23:18:49 +0530

    added simple code add,delete,render logic

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -1,6 +1,30 @@
+let todos = [];
+
 function addTodo() {
-    let divEl = document.createElement("div");
-    let parentEl = document.querySelector("body");
-    divEl.innerHTML = document.querySelector(".input").value;
-    parentEl.appendChild(divEl); 
-}
+    todos.push({
+        title : document.querySelector(".input-btn").value
+    });
+    renderTodo();
+
+}  
+
+    function createTodoComponent(todo) {
+        const divEl = document.createElement("div");
+        const spanEl = document.createElement("span");
+        spanEl.innerHTML = todo.title;
+        const buttonEl = document.createElement("button");
+        buttonEl.innerHTML = "Delete";
+        divEl.append(spanEl);
+        divEl.append(buttonEl);
+
+        return divEl;
+    }
+
+    // React
+    function renderTodo() {
+        document.querySelector(".todos").innerHTML = "";
+        for(let i = 0;i < todos.length;i++) {
+            const divEl = createTodoComponent(todos[i]);
+            document.querySelector(".todos").appendChild(divEl);
+        }
+    }
