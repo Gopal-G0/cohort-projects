e3317ce9942f9c80b2f80679e7647f9cc33b20ee
Gopal-G0 <heyitsmegopal@gmail.com>
2025-07-10 08:38:45 +0530

    added put,delete routes to server

--- hospitalServer/hospitalLogic.js
+++ hospitalServer/hospitalLogic.js
@@ -10,6 +10,8 @@ const users = [{
     }]
 }]
 
+app.use(express.json());
+
 app.get('/',function(req,res) {
     const gopalKidneys = users[0].kidneys;
     const numberOfKidneys = gopalKidneys.length;
@@ -29,18 +31,59 @@ app.get('/',function(req,res) {
 })
 
 app.post('/',function(req,res) {
-    const isHealthy = res.body.isHealthy;
+    const isHealthy = req.body.isHealthy;
     users[0].kidneys.push({
         healthy: isHealthy
+    })
+
+    res.json({
+        msg : "Done"
     })
 })
 
 app.put('/',function(req,res) {
-    
+    const gopalKidneys = users[0].kidneys;
+    const numberOfKidneys = gopalKidneys.length;
+    for(let i = 0;i < numberOfKidneys;i++) {
+        users[0].kidneys[i].healthy = true;
+    }
+
+    res.json({
+        Message: 'All Kidneys were transplanted successfully.'
+    })
 })
 
 app.delete('/',function(req,res) {
-    
+    const gopalKidneys = users[0].kidneys;
+    const numberOfKidneys = gopalKidneys.length;
+    let healthyKidneyCount = 0;
+    for(let i = 0;i < numberOfKidneys;i++) {
+        if(users[0].kidneys[i].healthy) {
+            healthyKidneyCount++;
+        }
+    }
+
+    let unhealthyKidneyCount = numberOfKidneys - healthyKidneyCount;
+    if(!unhealthyKidneyCount) {
+        res.status(411).json({
+            msg: "There are no unhealthy kidneys"
+        })
+    }
+
+    const newKidneys = [];
+    for(let i = 0;i < numberOfKidneys;i++) {
+        if(users[0].kidneys[i].healthy) {
+            newKidneys.push({
+                healthy: true
+            })
+        }
+    }
+
+    users[0].kidneys = newKidneys;
+    res.json({
+        msg: 'All Unhealthy Kidneys Removed'
+    })
+
 })
 
 app.listen(3000);
